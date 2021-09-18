6300809bbf3bf8c66a26de588d0af5fce565fba3
Aaron <arflierl@buffalo.edu>
2021-09-18 13:21:12 -0400

    Hello, World! python example

---
+++ main.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+
+def hey_you(noun):
+    greeting = "Hello, " + noun + "!"
+    print(greeting)
+
+if __name__ == '__main__':
+    noun = "World"
+    hey_you(noun)
