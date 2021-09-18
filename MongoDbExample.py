22211a54ca54ee19c095856cb89b76bcdde58ea0
mattcummings <mikmatt2001@gmail.com>
2021-09-18 15:03:57 -0400

    PyMongo sample code
    simple insert and find examples for pymongo/mongodb

---
+++ MongoDbExample.py
@@ -0,0 +1,23 @@
+import random
+import pymongo
+
+myClient = pymongo.MongoClient("mongodb://localhost:27017/")
+db = myClient["db"]
+numbers = db["numbers"]
+
+def addRandomNumber():
+    newNum = (random.random() * 100).__trunc__()
+    print("New Number: ", newNum)
+    numbers.insert_one({"num" : newNum})
+    return
+
+def showAllNums():
+    print("All numbers: ")
+    for num in numbers.find():
+        print(num['num'])
+    return
+
+if __name__ == '__main__':
+    addRandomNumber()
+    showAllNums()
+    print("Test")
