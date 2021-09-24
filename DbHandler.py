a15e8c0ff08b56742b4c97631b7ff2adcd8b325e
mattcummings <mikmatt2001@gmail.com>
2021-09-24 16:35:13 -0400

    Database functions for users #23

---
+++ DbHandler.py
@@ -0,0 +1,30 @@
+import pymongo
+import User as u
+
+myClient = pymongo.MongoClient("mongodb://mongo:27017/")
+db = myClient["db"]
+users = db["users"]
+
+def saveUser(user):
+    users.insert_one(user.asDict())
+
+def nameExists(name):
+    user = users.find({"username":name})
+    if len(list(user)) != 0:
+        return True
+    return False
+
+def getUser(name):
+    user = users.find_one({"username": name})
+    username = user["username"]
+    password = user["password"]
+    stats = user["stats"]
+
+    newUser = u.User(username, password)
+    newUser.stats = stats
+    return newUser
+
+def updateUser(user):
+    name = user.username
+    users.delete_one({"username": name})
+    saveUser(user)
