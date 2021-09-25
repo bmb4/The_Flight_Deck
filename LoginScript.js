c2499c90f718cdf347897fe314437225c9b32bf8
bmb4 <bmb4@buffalo.edu>
2021-09-25 14:33:59 -0400

    #2 Working on JS backend, added link to create new user page(inactive) at bottom of login page.

---
+++ LoginScript.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+let username = "bmb4";
+let password = "12345";
+
+//If I go the double page route this command may be useful
+//document.getElementById("demo").style.display = "block";
+
+
+function testy_bo_besty() {
+    document.getElementById()
+}
