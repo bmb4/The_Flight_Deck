2910c9ebd286dc41b5883bd2c29c95e63f22c1a9
jbseale <jbseale@buffalo.edu>
2021-10-07 12:52:26 -0400

    Connecting new account page to database #50

--- CreateAccountScript.js
+++ CreateAccountScript.js
@@ -1,9 +1,15 @@
+var MongoClient = require('mongodb').MongoClient;
+const dbpassword = process.env.DB_PASSWORD;
+const uri = "mongodb+srv://bmb4:"+dbpassword+"@Four-in-a-Sequence.3v48s.mongodb.net/DB?retryWrites=true&w=majority";
 
-function CreateAcc(){ 
+
+
+ function CreateAcc(){
+     console.log("creating account..")
     var user = document.getElementById("myform").elements.namedItem("username").value;
     var pass = document.getElementById("myform").elements.namedItem("password").value;
     var conpass = document.getElementById("myform").elements.namedItem("password_confirm").value;
-     if(user == ""){
+   /*   if(user == ""){
          alert("Please Enter Username");
          return false;
     }
@@ -16,12 +22,28 @@ function CreateAcc(){
     return false;
 }
     else{ 
+   
+
   document.writeln("Account has been created <br>");
   document.writeln("Your Username is:" + user);
   document.writeln("<br>Your Password is:" + pass);
-    }
-    
+    } */
+     MongoClient.connect(uri, function(err, DB) {
+        if (err) throw err;
+        // db pointing to newdb
+        
+        // document to be inserted
+        var doc = { name: "josh", password: "josh" };
+            
+        // insert document to 'users' collection using insertOne
+        DB.collection("Test").insertOne(doc, function(err, res) {
+            if (err) throw err;
+            console.log("Document inserted");
+            // close the connection to db when you are done with it
+            DB.close();
+        });
+    });
 
-}
-   
+} 
+
 
