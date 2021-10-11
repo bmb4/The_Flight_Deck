141ce318a6873bc1eb75110a8897a8408576381d
jbseale <jbseale@buffalo.edu>
2021-10-10 22:01:07 -0400

    Adding new accounts to database #50

--- CreateAccountScript.js
+++ CreateAccountScript.js
@@ -1,15 +1,71 @@
 var MongoClient = require('mongodb').MongoClient;
+
+
 const dbpassword = process.env.DB_PASSWORD;
-const uri = "mongodb+srv://bmb4:"+dbpassword+"@Four-in-a-Sequence.3v48s.mongodb.net/DB?retryWrites=true&w=majority";
+const url = "mongodb+srv://bmb4:"+dbpassword+"@Four-in-a-Sequence.3v48s.mongodb.net/DB?retryWrites=true&w=majority";
+var client = new MongoClient(url);
 
+async function AddAccount(){ 
+/* var user = document.getElementById("myform").elements.namedItem("username").value;
+ var pass = document.getElementById("myform").elements.namedItem("password").value;
+ var conpass = document.getElementById("myform").elements.namedItem("password_confirm").value;*/
+ var user = "test2" 
+ var pass = "test"
+ var conpass = "test"
+  try {
+    await client.connect();
+    
+    const database = client.db("DB");
+    const db = database.collection("Test");
+    console.log("creating account..")
+    // create a document to insert
+    const doc = {
+      Username: user,
+      Password: pass,
+    }
+    const find =  await db.find({"Username":user}).count()
+    if(find == 0){
+    var v = CheckAcc(user,pass,conpass);
+    if(v == true){
+    const result = await db.insertOne(doc);
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    }
+  }
+  else{
+    
+    console.log("Account Already exists");
+  }
+  } finally {
+    await client.close();
+  }
+}
 
+async function FindAccount(username){ 
 
- function CreateAcc(){
-     console.log("creating account..")
+    try {
+      await client.connect();
+      
+      const database = client.db("DB");
+      const db = database.collection("Test");
+      console.log("looking for account..")
+      // create a document to insert
+      const doc = {
+        Username: username,
+      }
+      const result = await db.findOne(doc);
+      console.log(result);
+    } finally {
+      await client.close();
+    }
+  }
+
+AddAccount().catch(console.dir);
+
+/*  function CreateAcc(){
     var user = document.getElementById("myform").elements.namedItem("username").value;
     var pass = document.getElementById("myform").elements.namedItem("password").value;
     var conpass = document.getElementById("myform").elements.namedItem("password_confirm").value;
-   /*   if(user == ""){
+      if(user == ""){
          alert("Please Enter Username");
          return false;
     }
@@ -27,23 +83,23 @@ const uri = "mongodb+srv://bmb4:"+dbpassword+"@Four-in-a-Sequence.3v48s.mongodb.
   document.writeln("Account has been created <br>");
   document.writeln("Your Username is:" + user);
   document.writeln("<br>Your Password is:" + pass);
-    } */
-     MongoClient.connect(uri, function(err, DB) {
-        if (err) throw err;
-        // db pointing to newdb
-        
-        // document to be inserted
-        var doc = { name: "josh", password: "josh" };
-            
-        // insert document to 'users' collection using insertOne
-        DB.collection("Test").insertOne(doc, function(err, res) {
-            if (err) throw err;
-            console.log("Document inserted");
-            // close the connection to db when you are done with it
-            DB.close();
-        });
-    });
+    } 
+   
+}  */
 
+function CheckAcc(username,password,password2){
+  var x = true;
+   if(username == ""){
+      alert("Please Enter Username");
+      x = false;
+ }
+ if(password == ""){
+     alert("Please Enter Password");
+     x = false;
+}
+if(password != password2){
+ alert("Passwords do not match");
+ x =  false;
+}
+ return x
 } 
-
-
