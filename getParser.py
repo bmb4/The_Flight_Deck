c767646aea629d2f9480c9bbd526082151cf545b
paolo-Rancati <52716043+paolo-Rancati@users.noreply.github.com>
2021-10-16 18:52:30 -0400

    #51 updated getParser.py w/ homescreen and leaderboard

--- getParser.py
+++ getParser.py
@@ -7,10 +7,18 @@ def getHandler(self, request):
     path = util.getPath(request[0])
     print(path)
     if path == "":
-        content = util.getFile("templates/Login Page.html")
+        content = util.getFile("templates/homeScreen.html")
         return responses.create200(content, "text/html", len(content))
+    elif path == "login":
+        content = util.getFile("templates/Login Page.html")
+        return responses.create200(content, "text/html", len(content))        
+    elif path == "LandingPage":
+        content = util.getFile("templates/Landing Page.html")
+        return responses.create200(content, "text/html", len(content))
+    elif path == "leaderboard":
+        content = util.getFile("templates/leaderboard.html)
+        return responses.create200(content, "text/html", len(content))        
     elif path == "static":
         content = util.getFile("templates/static/WebsiteCSS.css")
         return responses.create200(content, "text/css", len(content))
     return responses.create404("Content not found.", "text/plain", 18)
-
