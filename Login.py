8a477e42bca157453a9951e18570ee5def888dfe
bmb4 <bmb4@buffalo.edu>
2021-10-18 13:04:19 -0400

    Worked on Login to dtb script

--- Login.py
+++ Login.py
@@ -16,9 +16,9 @@ test = db["Test"]
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "really secret key"
 
-@app.route('/')
-def index():
-    return render_template('Login page.html')
+#@app.route('/')
+#def index():
+#    return render_template('Login page.html')
 
    
 @app.route('/Login Page',methods = ['POST'])
@@ -27,16 +27,15 @@ def login():
     password = request.form['password']
     userlist = test.find({"username":username})
     FailMessage = ("Invalid username or password")
-    SuccessMsg = ("Login Success")
     if len(list(userlist)) == 0:
-        mydict = { "username": username, "password": password}
-        user = test.insert_one(mydict)
         return FailMessage
+    elif password != userlist[0]['password']:
+        return FailMessage
+    elif password ==  userlist[0]['password']:
+            #route to landing or profile page
+            return 
     else:
-        print(len(list(userlist)))
-        flash('Username is already taken')
-        #return render_template('Login Page.html')
-        return SuccessMsg 
+        return ("unknown error")
 
 
 if __name__ == '__main__':
