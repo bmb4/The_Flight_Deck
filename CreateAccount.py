428b71198c676ade52f5768d0a45d43936bc13cc
mattcummings <mikmatt2001@gmail.com>
2021-10-19 16:11:00 -0400

    Changed Create Account Database #50

--- CreateAccount.py
+++ CreateAccount.py
@@ -2,13 +2,14 @@ from logging import NullHandler
 import os
 import pymongo
 from flask import Flask, request, render_template, flash
+import User
 
 password = os.environ.get('DB_PASSWORD')
 
 client = pymongo.MongoClient("mongodb+srv://bmb4:"+password+"@Four-in-a-Sequence.3v48s.mongodb.net/DB?retryWrites=true&w=majority")
 
 db = client["DB"]
-test = db["Test"]
+UserAccounts = db["UserAccounts"]
 
 
 
@@ -27,13 +28,13 @@ def createaccount():
     password = request.form['password']
     passsword2 = request.form['password_confirm']
     Message =("Account Created!!!<br> username: "+username+"<br>password: "+ password)
-    userlist = test.find({"username":username})
+    userlist = UserAccounts.find({"username":username})
     if password != passsword2:
         flash('Passwords do not match')
         return render_template('CreateAccount.html') 
     if len(list(userlist)) == 0:
-        mydict = { "username": username, "password": password}
-        user = test.insert_one(mydict)
+        newUser = User.User(username, password)
+        UserAccounts.insert_one(newUser.asDict())
         return Message
     else:
         print(len(list(userlist)))
