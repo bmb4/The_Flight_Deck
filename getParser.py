45a9919346c44e6a8bcafb1a988b4faab5005b0d
paolo-Rancati <52716043+paolo-Rancati@users.noreply.github.com>
2021-11-06 19:09:21 -0400

    #78 added GamePage to getParser

--- getParser.py
+++ getParser.py
@@ -41,6 +41,9 @@ def getHandler(self, request):
     elif path == "profileScript.js":
         content = util.getFile("profileScript.js")
         return responses.create200(content, "text/javascript", len(content))
+    elif path == "newgame":
+        content = util.getFile("templates/GamePage.html")
+        return responses.create200(content, "text/html", len(content))    
     elif path == "fourSeq.js":
         content = util.getFile("fourSeq.js")
         return responses.create200(content, "text/javascript", len(content))
