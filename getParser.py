2b84d85e294803c0399bd7fef433415cb0fd6190
paolo-Rancati <52716043+paolo-Rancati@users.noreply.github.com>
2021-11-07 21:06:21 -0500

    #78 lovey typo

--- getParser.py
+++ getParser.py
@@ -46,7 +46,7 @@ def getHandler(self, request):
     elif path == "NewGame":
         content = util.getFile("templates/gamePage.html")
         return responses.create200(content, "text/html", len(content))    
-    elift path == "fourSeq.js":
+    elif path == "fourSeq.js":
         content = util.getFile("templates/fourSeq.js")
         return responses.create200(content, "text/javascript", len(content))
     elif path == "profileScript.js":
