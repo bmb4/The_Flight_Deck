f1246a2b08b96e7ea3fe0ae79b31b9395b369e45
mattcummings <mikmatt2001@gmail.com>
2021-11-11 15:27:00 -0500

    Add if statement to check data #80

--- functions.js
+++ functions.js
@@ -1,8 +1,9 @@
 var	request	= new XMLHttpRequest();
 request.onreadystatechange	=	function(){
     if	(this.readyState === 4 && this.status === 200){
-        alert(this.responseText)
-        window.location.href = "NewGame"
+        if(this.responseText === "NewGame"){
+              window.location.href = "NewGame"
+        }
     }
 };
 
