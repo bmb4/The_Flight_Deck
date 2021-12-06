fdbdd118dd04b769a062759312bf3114bec0a30c
megan9572 <megan9572@gmail.com>
2021-12-06 02:11:52 -0500

    removed setSession.js and inSession.php, and any related code #105

--- profileScript.js
+++ profileScript.js
@@ -12,22 +12,6 @@ var user = {
 const _green = "rgb(0, 215, 0)";
 const _black = "#2b2929";
 const _blue = "#3F5CF3";
-// BACKUP 3
-// ALL PROMPTS, DISPLAY BASED ON INPUT
-//$(document).ready(function() {
-//    var prompts = {"username":'Unknown User','Games Played':'40','Wins':'15','Losses':'20','Draws':'5'};
-//    for (var key of Object.keys(prompts)){
-//        input = prompt("Please enter "+key, '');
-//        if (input == null || input == '') {
-//            alert("Prompt cancelled, using default value for "+key);
-//            input = prompts[key]
-//        }
-//        $('#'+key).append(input);
-//    }
-//    input = prompt("[1]Cats or [2]ducks?",'type number');
-//    if (input == '2'){user["profilePic"] = "../images/duck.png";}
-//    displayPic();
-//});
 
 // BACKUP 2
 // GRAB FROM DB VIA PROMPT OF INPUT USERNAME
@@ -42,42 +26,6 @@ $(document).ready(function () {
         }
     });
 });
-
-// BACKUP !
-// STORING CURRENT USER VIA CLIENT ADDRESSES
-//$(document).ready(function() {
-//    $.ajax({
-//        type: 'GET',
-//        url: '/get_stats',
-//        dataType: 'text',
-//        success: function(data){
-//            text = JSON.parse(data);        // {"username": self.username, "password": self.password, "stats": self.stats}
-//            alert(text);
-//        }
-//    });
-//});
-
-// IDEAL
-// STORING CURRENT USER VIA SESSION VARIABLES
-//$(document).ready(function(){
-//    $.ajax({
-//        type: 'GET',
-//        url: 'inSession.php',
-//        dataType: 'text',
-//        success: function(data){
-//            text = data;
-//            alert(data);
-//            $.ajax({
-//                url: '/get_stats',
-//                success: function(info){
-//                    var d = JSON.parse(info);
-//                    for (let i = 0; i < d; i++) {if (d[i]['username']==data) {updateStats(d[i]);}}
-//                    updateStats();
-//                }
-//            });
-//        }
-//    });
-//});
 
 
 function updateStats(data) {
